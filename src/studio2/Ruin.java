74f7be36b0b96308579c6d951d95252a11fa799e
Yael <112411257+YaelM13@users.noreply.github.com>
2024-02-01 10:12:13 -0600

    just starting

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,23 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Starting Amount: ");
+		int startamount = scan.nextInt();
+		
+		System.out.print("Win Probability: ");
+		int winchance = scan.nextInt();
+		
+		System.out.print("Win Limit: ");
+		int winlimit = scan.nextInt();
+		
+		
+
+	}
+
+}
